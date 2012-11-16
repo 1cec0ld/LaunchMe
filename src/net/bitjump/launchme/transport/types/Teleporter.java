dd0dbdf8faf4273a786e2804e1f758059c27a38b
Daniel Mudd <spudstabber@gmail.com>
2012-11-16 11:43:12 +0000

    Fix for teleporters...

--- src/net/bitjump/launchme/transport/types/Teleporter.java
+++ src/net/bitjump/launchme/transport/types/Teleporter.java
@@ -31,7 +31,7 @@ public class Teleporter extends BasicTransport
 		String[] lines = s.getLines();
 		String[] coords = lines[1].split(",");
 		
-		if(p.getLocation().getBlock().getRelative(0, -1, 0).getTypeId() == LaunchMe.config.getInt("transports.teleporter.deactivate"))
+		if(p.getLocation().getBlock().getRelative(0, -1, 0).getTypeId() != LaunchMe.config.getInt("transports.teleporter.deactivate"))
 		{
 			final Location end = new Location(Bukkit.getWorld(lines[3]), Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Double.parseDouble(coords[2]));
 		
