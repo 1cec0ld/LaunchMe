1f9cc349cdce86e4016cda05951ef38c370d8119
Daniel Mudd <spudstabber@gmail.com>
2012-11-17 23:03:41 +0000

    Temporary removal of the logging filter til I find out why it's a big silly.

--- src/net/bitjump/launchme/KickFilter.java
+++ src/net/bitjump/launchme/KickFilter.java
@@ -9,6 +9,7 @@ public class KickFilter implements Filter
 	public boolean isLoggable(LogRecord arg0) 
 	{
 		boolean isLoggable = true;
+		
 		if(arg0.getMessage().endsWith("floating too long!"));
 		{
 			isLoggable = false;
