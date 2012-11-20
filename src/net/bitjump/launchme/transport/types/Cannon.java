133cf7d0778ebe9d8903184b1ef65daf03f63722
Daniel Mudd <spudstabber@gmail.com>
2012-11-20 18:43:46 +0000

    Allowed for setting of height from cannons.

--- src/net/bitjump/launchme/transport/types/Cannon.java
+++ src/net/bitjump/launchme/transport/types/Cannon.java
@@ -34,7 +34,7 @@ public class Cannon extends BasicTransport
 		
 		if(p.getLocation().getBlock().getRelative(0, -1, 0).getTypeId() != LaunchMe.config.getInt("transports.cannon.deactivate"))
 		{
-			final Location end = new Location(Bukkit.getWorld(lines[3]), Double.parseDouble(coords[0]), 800, Double.parseDouble(coords[1]));
+			final Location end = new Location(Bukkit.getWorld(lines[3]), Double.parseDouble(coords[0]), LaunchMe.config.getInt("transports.cannon.height", 600), Double.parseDouble(coords[1]));
 			
 			if(LaunchMe.econ)
 			{
